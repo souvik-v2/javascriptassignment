1bbd29a700c764ea02e6c5f881384207e880a38b
Souvik Patra <souvik.patra@v2solutions.com>
2021-06-15 15:56:33 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+
+function calculate(operator) {
+
+    const obj = {
+        number: parseInt(document.getElementById('number').value)
+    };
+    
+    Object.defineProperty(obj, "reset", {
+      get : function () {this.counter = 0;}
+    });
+    
+    Object.defineProperty(obj, "increment", {
+      get : function () { document.getElementById('number').value = this.number+1;}
+    });
+    
+    Object.defineProperty(obj, "decrement", {
+      get : function () {document.getElementById('number').value =this.number-1;}
+    });
+    
+    Object.defineProperty(obj, "mul", {
+      get : function () {document.getElementById('number').value =this.number*this.number;}
+    });
+    
+    Object.defineProperty(obj, "div", {
+      get : function () {document.getElementById('number').value = this.number/this.number;}
+    });
+
+
+    if(operator == '+')
+        obj.increment;
+    else if(operator == '-')
+        obj.decrement;
+    else if(operator == '*')
+        obj.mul;
+    else if(operator == '/')
+        obj.div;
+    else
+        obj.reset;
+}
